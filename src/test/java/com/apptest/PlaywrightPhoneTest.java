912201b4b8fed011e22d66c8c506bdd5947f9fa0
Cristea3051 <cristeavictor4@gmail.com>
2025-04-07 17:21:25 +0300

    dsadsada

---
+++ src/test/java/com/apptest/PlaywrightPhoneTest.java
@@ -0,0 +1,31 @@
+package com.apptest;
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.ColorScheme;
+
+import java.nio.file.Paths;
+
+public class PlaywrightPhoneTest{
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false)); // Headless off
+            BrowserContext context = browser.newContext(new Browser.NewContextOptions()
+                    .setDeviceScaleFactor(3)
+                    .setViewportSize(1170, 2532)
+                    .setUserAgent("Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Mobile/15E148 Safari/604.1")
+                    .setGeolocation(-23.5489, -46.6388) // São Paulo, Brazilia
+                    .setPermissions(java.util.Arrays.asList("geolocation"))
+                    .setColorScheme(ColorScheme.DARK)
+                    .setIsMobile(true)
+                    .setHasTouch(true));
+
+            Page page = context.newPage();
+            page.navigate("https://www.google.com");
+            page.locator("[name='q']").fill("Wikipedia");
+            page.locator("[name='q']").press("Enter");
+            page.waitForTimeout(5000);
+            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("google_search_iphone_brazil.png")));
+
+            browser.close();
+        }
+    }
+}
